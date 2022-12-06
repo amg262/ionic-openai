c99090f1f9cf459d67ab48b40ae664862de3dd0b
Andrew Gunn <andrewmgunn26@gmail.com>
2022-12-05 20:58:49 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'my.app',
+  appName: 'my-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
